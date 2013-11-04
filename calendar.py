42e27d1f15cf5fa2ee9023ef42e40d769689519e
Juho Vepsalainen <bebraw@gmail.com>
2013-11-04 20:50:10 +0200

    Operate on 1 and 0 instead of True and False
    More convenient this way.

--- calendar.py
+++ calendar.py
@@ -2,8 +2,8 @@ from datetime import date
 import itertools
 
 
-def to_booleans(arr):
-    return map(lambda a: True if a == '1' else False, arr)
+def to_ints(arr):
+    return map(int, arr)
 
 def splice(arr, amt):
     ret = list()
@@ -18,7 +18,7 @@ def true_for_all(days):
     capture = map(all, zip(*weeks))
 
     return list(itertools.chain(*map(lambda week: map(
-        lambda v: v[1] and capture[v[0] % 7], enumerate(week)),
+        lambda v: 1 if v[1] and capture[v[0] % 7] else 0, enumerate(week)),
         weeks)))
 
 def true_for_week(trueAll, firstDate):
@@ -27,14 +27,14 @@ def true_for_week(trueAll, firstDate):
     return trueAll[offset: offset + 7]
 
 def true_for_some(days, trueAll):
-    return map(lambda (a, b): a and not b, zip(days, trueAll))
+    return map(lambda (a, b): 1 if a and not b else 0, zip(days, trueAll))
 
 def get_date(str):
     return date(*map(int, str.split('-')))
 
 def main():
     firstDate = get_date('2013-11-03')
-    days = to_booleans(list('10010010100001'))
+    days = to_ints(list('10010010100001'))
     overlaps = true_for_all(days)
     sub = true_for_some(days, overlaps)
     weekOverlaps = true_for_week(overlaps, firstDate)
