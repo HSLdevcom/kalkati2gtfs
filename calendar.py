b2aa7ec5d509884dc6b5083f4a8e780f01156a96
Juho Vepsalainen <bebraw@gmail.com>
2013-11-04 20:18:29 +0200

    Make calendar API day based instead of week

--- calendar.py
+++ calendar.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def to_booleans(arr):
     return map(lambda a: True if a == '1' else False, arr)
 
@@ -9,21 +12,23 @@ def splice(arr, amt):
 
     return ret
 
-def true_for_all(weeks):
-    return map(all, zip(*weeks))
+def true_for_all(days):
+    weeks = splice(days, 7)
+    capture = map(all, zip(*weeks))
 
-def true_for_some(weeks, trueAll):
-    return map(lambda week: map(
-        lambda v: v[1] and not trueAll[v[0] % 7], enumerate(week)),
-        weeks)
+    return list(itertools.chain(*map(lambda week: map(
+        lambda v: v[1] and capture[v[0] % 7], enumerate(week)),
+        weeks)))
+
+def true_for_some(days, trueAll):
+    return map(lambda (a, b): a and not b, zip(days, trueAll))
 
 def main():
-    a = to_booleans(list('10010010100001'))
-    weeks = splice(a, 7)
-    overlaps = true_for_all(weeks)
-    sub = true_for_some(weeks, overlaps)
+    days = to_booleans(list('10010010100001'))
+    overlaps = true_for_all(days)
+    sub = true_for_some(days, overlaps)
 
-    print 'weeks', weeks
+    print 'days', days
     print 'overlaps', overlaps
     print 'sub', sub
 
