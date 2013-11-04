27f0dc463a753a0f5f3aa9eb727d6eab02b80f68
Juho Vepsalainen <bebraw@gmail.com>
2013-11-04 20:46:24 +0200

    Add function to get week overlaps

--- calendar.py
+++ calendar.py
@@ -1,3 +1,4 @@
+from datetime import date
 import itertools
 
 
@@ -20,16 +21,28 @@ def true_for_all(days):
         lambda v: v[1] and capture[v[0] % 7], enumerate(week)),
         weeks)))
 
+def true_for_week(trueAll, firstDate):
+    offset = firstDate.weekday()
+
+    return trueAll[offset: offset + 7]
+
 def true_for_some(days, trueAll):
     return map(lambda (a, b): a and not b, zip(days, trueAll))
 
+def get_date(str):
+    return date(*map(int, str.split('-')))
+
 def main():
+    firstDate = get_date('2013-11-03')
     days = to_booleans(list('10010010100001'))
     overlaps = true_for_all(days)
     sub = true_for_some(days, overlaps)
+    weekOverlaps = true_for_week(overlaps, firstDate)
 
+    print firstDate.weekday() # should be 6 for Sunday
     print 'days', days
     print 'overlaps', overlaps
+    print 'week overlaps', weekOverlaps
     print 'sub', sub
 
 if __name__ == '__main__':
